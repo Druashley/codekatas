203f312bfe0ac8941c545475406c24fe264edd51
Dru <druashley@gmail.com>
2021-12-14 09:25:20 -0800

    place to post code katas.

---
+++ index.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/57cc975ed542d3148f00015b/solutions/javascript
+
+// function check(a, x) {
+//   if (a.indexOf(x) !== -1) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(check([66, 101], 66));
